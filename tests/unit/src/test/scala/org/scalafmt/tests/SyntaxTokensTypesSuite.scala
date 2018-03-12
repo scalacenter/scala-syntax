0eeee695c4ca02adf96809adca73895b33bd584b
Guillaume Massé <masgui@gmail.com>
2018-03-12 17:15:29 +0100

    SyntaxTokens Type.With

--- tests/unit/src/test/scala/org/scalafmt/tests/SyntaxTokensTypesSuite.scala
+++ tests/unit/src/test/scala/org/scalafmt/tests/SyntaxTokensTypesSuite.scala
@@ -9,6 +9,10 @@ object SyntaxTokensTypesSuite extends SyntaxTokensSuiteUtils {
   checkOneType[Type.Select](_.tokensDot)("A→.←B")
   // checkOneType[Type.ImplicitFunction](_.tokensImplicit)("→implicit← A => B")
   // checkOneType[Type.ImplicitFunction](_.tokensRightArrow)("implicit A →=>← B")
-  
+  // checkOneType[Type.And](_.tokensAnd)("A →&← B")
+  // checkOneType[Type.Or](_.tokensOr)("A →|← B") dotty
+  checkOneType[Type.With](_.tokensWith)("A →with← B")
+
+
   // →←
 }
