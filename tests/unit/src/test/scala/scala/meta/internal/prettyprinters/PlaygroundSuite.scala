c7cff63a19dce7214dcc2876f2627fcdb056193c
Guillaume Massé <masgui@gmail.com>
2018-09-26 10:27:17 +0200

    Add PlaygroundSuite

--- tests/unit/src/test/scala/scala/meta/internal/prettyprinters/PlaygroundSuite.scala
+++ tests/unit/src/test/scala/scala/meta/internal/prettyprinters/PlaygroundSuite.scala
@@ -5,25 +5,24 @@ import java.nio.file.Paths
 
 object PlaygroundSuite extends BaseScalaPrinterTest {
 
-  val cover = true
-  // val cover = false
+  // val cover = true
+  // // val cover = false
 
-
-  if (cover) {
-    val coverageFile = Paths.get("tests/slow/coverage-comments.txt").toFile
-    // val regressionPath = Paths.get("tests/slow/regressions-comments.txt").toFile
-    val todoFile = coverageFile
-    val source = scala.io.Source.fromFile(todoFile)
-    val lines = source.getLines
-    lines.drop(4)
-    val file = lines.next
+  // if (cover) {
+  //   val coverageFile = Paths.get("tests/slow/coverage-comments.txt").toFile
+  //   val todoFile = coverageFile
+  //   val source = scala.io.Source.fromFile(todoFile)
+  //   val lines = source.getLines
+  //   lines.drop(4)
+  //   val file = lines.next
     
-    source.close()
-    val base = Paths.get("/home/gui/scala-syntax/tests/slow/target/repos")
-    val input = Input.File(AbsolutePath(base.resolve(file)))
-    checkComments("playground slow", input, true)  
-  } else {
-    checkComments("playground resource", resource("playground.scala"))
-    // checkSource(resource("playground.scala").text)
-  }
+  //   source.close()
+  //   val base = Paths.get("/home/gui/scala-syntax/tests/slow/target/repos")
+  //   val input = Input.File(AbsolutePath(base.resolve(file)))
+  //   checkComments("playground slow", input, true)  
+  // } else {
+  //   checkComments("playground resource", resource("playground.scala"))
+  //   // checkSource(resource("playground.scala").text)
+  // }
+  check("1")
 }
